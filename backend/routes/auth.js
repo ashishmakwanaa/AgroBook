ddebfa02d55c715e528d82c07b5d9c98fd97a5f7
Mandeepsinh Gohil <gohilmandipsinh242@gmail.com>
2023-08-04 22:06:48 +0530

    login authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,9 @@ const User = require("../models/User.js")
 const { body, validationResult } = require('express-validator');
 
 
-//Create a User and store in database using :POST "/auth/createuser". it Does Not require authentication
+
+
+//ROUTE 1 : Create a User and store in database using :POST "/auth/createuser". it Does Not require authentication
 router.post("/createuser", [
 
     //for validation using express-validator packge
@@ -13,13 +15,10 @@ router.post("/createuser", [
     body("password", "Password length must be atlist six character").isLength({ min: 6 })
 
 ], async (req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
 
     //destructure req.body
     const { name, email, password } = req.body;
-    console.log(name)
-    console.log(email)
-    console.log(password)
 
     //if userDetail is not follow above validation then there is an error
     const error = validationResult(req)
@@ -46,11 +45,68 @@ router.post("/createuser", [
             password: password,
             email: email
         })
-        res.status(200).json({ message: "Successfully signup" })
+        return res.status(200).json({ message: "Successfully signup" })
     } catch (error) {
         console.log(error.message)
         res.status(500).json({ message: "Internal server error" });
     }
 })
+
+
+
+
+//ROUTE 2 : Authenticate a user  :POST "/auth/login". it Does Not require login
+router.post("/login", [
+
+    //for validation using express-validator packge
+    body('email', "Enter a valid Email").isEmail(),
+    body("password", "password can not be blank").exists()
+
+], async (req, res) => {
+
+
+    //for email and password validation 
+    //if email is not in form or password is empty then do not move ahead and just give above error
+    const error = validationResult(req)
+
+    if (!error.isEmpty()) {
+        // console.log("jmj")
+        const errorMessages = error.array().map(error => error.msg);
+        const message = errorMessages[0];
+        console.log(message)
+        return res.status(400).json({ message })
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+    try {
+
+        //find user from the database by email
+        let user = await User.findOne({ email });
+
+        //if user not exist then send return error to client side
+        if (!user) {
+            return res.status(400).json({ message: "Sorry an user with this email is already exist" })
+        }
+
+        //if user exist then check password
+
+        if (password != user.password) {
+            return res.status(400).json({ message: "please Enter correct creadencial - password incorrect" })
+        }
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+
+        return res.status(200).json({ message: "Successfully Loged In" })
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ message: "Internal server error" });
+    }
+})
+
 
 module.exports = router
--- frontend/src/component/SignupPage.js
+++ frontend/src/component/SignupPage.js
@@ -18,19 +18,22 @@ const SignupPage = () => {
     }
   );
 
-  async function storeData() {
+  async function storeUser() {
     const url = "http://localhost:4000/auth/createuser";
+    setLoading(true)
 
     const res = await fetch(url, {
       method: 'POST',
       headers: {
-        'Content-type': 'application/json',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(formData)
     });
 
-    setLoading(false);
     const data = await res.json();
+
+    console.log(data["message"])
+    console.log(res.ok)
 
     if (res.ok) {
       swal({
@@ -40,14 +43,14 @@ const SignupPage = () => {
         timer: 3000
       })
 
-
       setFormData({
         name: "",
         email: "",
         password: "",
         confirmPassword: ""
       });
-      navigate("./Login")
+      navigate("/login")
+
     } else {
       swal({
         title: data["message"],
@@ -56,34 +59,35 @@ const SignupPage = () => {
         timer: 3000
       })
     }
+    setLoading(false)
 
   }
 
 
   const handleSubmit = (e) => {
+    try {
+      e.preventDefault()
+      const password = formData.password;
+      const conformPassword = formData.confirmPassword;
 
-    e.preventDefult();
-    const password = formData.password;
-    const conformPassword = formData.confirmPassword;
-
-    if (password !== conformPassword) {
-
-      swal({
-        title: "Password Not Match",
-        icon: "error",
-        button: false,
-        timer: 3000
-      })
-      setFormData({ formData })
-    } else {
-      setLoading(true);
-      storeData();
-      setLoading(false);
+      if (password != conformPassword) {
+        swal({
+          title: "Password Not Match",
+          icon: "error",
+          button: false,
+          timer: 3000
+        })
+      }
+      else {
+        storeUser();
+      }
+    } catch (err) {
+      window.alert(err)
     }
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center  justify-center">
       <video
         className="absolute top-0 left-0 object-cover w-full h-full opacity-80 z-0"
         loop
@@ -96,16 +100,16 @@ const SignupPage = () => {
         />
       </video>
 
-      <div className="w-full max-w-lg p-8 bg-white rounded-lg shadow-lg z-10">
+      <div className="w-full opacity-80 max-w-lg p-8 bg-white rounded-lg shadow-lg z-10">
         <h2 className="text-2xl text-center font-semibold mb-6"><span className='text-green-600 no-underline hover:underline hover:cursor-pointer'>Grow With Us,</span> Sign Up Today!</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
             id="name"
             name="name"
             value={formData.name}
             onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-            className="block w-full px-4 py-3 mb-4 border rounded-lg placeholder-gray-400 bg-gray-50 text-gray-800"
+            className="block w-full px-4 py-3 mb-4 border rounded-lg placeholder-gray-600 bg-slate-200 text-gray-800"
             placeholder="Enter Your Name"
             required
           />
@@ -115,7 +119,7 @@ const SignupPage = () => {
             name="email"
             value={formData.email}
             onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-            className="block w-full px-4 py-3 mb-4 border rounded-lg placeholder-gray-400 bg-gray-50 text-gray-800"
+            className="block w-full px-4 py-3 mb-4 border rounded-lg placeholder-gray-600 bg-slate-200 text-gray-800"
             placeholder="Enter Your Email"
             required
           />
@@ -125,7 +129,7 @@ const SignupPage = () => {
             name="password"
             value={formData.password}
             onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-            className="block w-full px-4 py-3 mb-4 border rounded-lg placeholder-gray-400 bg-gray-50 text-gray-800"
+            className="block w-full px-4 py-3 mb-4 border rounded-lg placeholder-gray-600 bg-slate-200 text-gray-800"
             placeholder="Enter Password"
             required
           />
@@ -134,19 +138,17 @@ const SignupPage = () => {
             name="confirmPassword"
             value={formData.confirmPassword}
             onChange={(e) => setFormData({ ...formData, confirmPassword: e.target.value })}
-            className="block w-full px-4 py-3 mb-6 border rounded-lg placeholder-gray-400 bg-gray-50 text-gray-800"
+            className="block w-full px-4 py-3 mb-6 border rounded-lg placeholder-gray-600 bg-slate-200 text-gray-800"
             placeholder="Conform Password"
             required
           />
-          {/* <button
-            type="submit"
-            className="w-full py-3 text-white bg-green-500 rounded-lg hover:bg-green-600 transition duration-300"
-          >
-            Sign Up
-          </button> */}
-          <div className="p-2 w-full">
-            <button onClick={handleSubmit} className="w-full  place-items-center py-3 bg-green-500 rounded-lg text-white hover:bg-green-600  transition  duration-300 border-0"><span className='text-lg '></span>
-              {loading ? <TailSpin height={25} className=" place-items-center align-middle" color='white' /> : "SignUp"}
+          <div className="p-2 w-full ">
+            <button className="w-full  place-items-center py-3 bg-green-500 rounded-lg text-white hover:bg-green-600  transition  duration-300 border-0"><span className='text-lg '></span>
+              {loading ?
+                <div className='flex items-center justify-center '>
+                  <TailSpin height={25} color='white' />
+                </div>
+                : "SignUp"}
             </button>
           </div>
         </form>
