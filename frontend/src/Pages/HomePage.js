192c7ec2d512009ed8c73c1ad138b12e8c5198c7
ashishmakwanaa <ashishmak2406@gmail.com>
2023-08-26 18:04:52 +0530

    service page change

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Navbar from '../component/Navbar'
 import CarouselComponent from '../component/CarouselComp';
 import AboutUs from '../component/About';
-// import Service from '../component/Service';
-
+import Service from '../component/Service';
 
 const HomePage = () => {
   const links = [
@@ -21,38 +20,17 @@ const HomePage = () => {
       Step into the future of agriculture with Agrobook. We invite you to explore a realm of possibilities, where tradition meets innovation and expertise meets technology. Your journey to unparalleled agricultural success begins here.
     </div>
   ];
-  const services = [
-    {
-      title: 'Service 1',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      // imageSrc: Image1,
-    },
-    {
-      title: 'Service 2',
-      description: 'Nullam ultricies arcu vel odio hendrerit, vel elementum neque facilisis.',
-      // imageSrc: Image2,
-    },
-    {
-      title: 'Service 3',
-      description: 'Nullam ultricies arcu vel odio hendrerit, vel elementum neque facilisis.',
-      // imageSrc: Image3,
-    },
-    // Add more services
-  ];
+
 
   const[showAbout,setshowAbout]=useState(false);
-  // const [showServices, setShowServices] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       const aboutSection = document.querySelector('.bg-green-400');
-      const servicesSection = document.querySelector('.services-section');
       if (aboutSection && window.scrollY > aboutSection.offsetTop - window.innerHeight / 2) {
         setshowAbout(true);
       }
-      // if (servicesSection && window.scrollY > servicesSection.offsetTop - window.innerHeight / 2) {
-      //   setShowServices(true);
-      // }
+     
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -72,16 +50,7 @@ const HomePage = () => {
       <div>
       <AboutUs show={showAbout}/>
       </div>
-      {/* <div className="services-section bg-gray-100">
-        {services.map((service, index) => (
-          <Service
-            key={index}
-            index={index}
-            show={showServices}
-            {...service} 
-          />
-        ))}
-      </div>  */}
+      <Service/>
 
     </>
 
